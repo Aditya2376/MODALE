3fe505e941fabecc11027e9d859bc3b9f5d544ed
Aditya2376 <109151812+Aditya2376@users.noreply.github.com>
2023-02-21 16:49:20 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,13 @@
+/*window.addEventListener('scroll', function() {
+  var element = document.getElementsByClassName('slider-content-scroll')[0];
+  var bounding = element.getBoundingClientRect();
+
+  if (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  ) {
+    element.classList.add('animate');
+  }
+});*/
